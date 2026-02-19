012b70b9358cc464282db0ad70c88b9679214795
mmh914 <107807417+mmh914@users.noreply.github.com>
2026-02-18 22:05:12 -0500

    basic pages

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-
+// Highlight current page in the nav
+(() => {
+  const path = location.pathname.split("/").pop() || "index.html";
+  document.querySelectorAll("nav a").forEach((a) => {
+    if (a.getAttribute("href") === path) a.classList.add("active");
+  });
+})();
